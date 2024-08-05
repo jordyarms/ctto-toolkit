bc4e7c3f2e7e0017b9f2068dfd4b453bb53d2f7c
jordyarms <jordan@addwonder.com>
2024-08-05 17:44:38 -0400

    corrections

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://jordyarms.github.io/ctto-toolkit',
+  url: 'https://jordyarms.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ctto-toolkit/',
